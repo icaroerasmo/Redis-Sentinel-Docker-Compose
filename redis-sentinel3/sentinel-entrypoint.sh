ba6b68919c12bdf716a0004e2f4259fe96c93a5b
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2024-03-11 01:20:20 -0300

    fixes configs for to demand password

--- redis-sentinel3/sentinel-entrypoint.sh
+++ redis-sentinel3/sentinel-entrypoint.sh
@@ -4,4 +4,4 @@ sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /redis/sentinel.conf
  
-redis-server /redis/sentinel.conf --sentinel
+redis-server /etc/redis/redis.conf /redis/sentinel.conf --sentinel
